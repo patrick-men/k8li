6e791be6d69f22f73e49554ada0203bd23311bbc
Patrick Palma Mendes <patrick-mendes@bluewin.ch>
2024-07-07 10:33:24 +0200

    fix: now runnable with alias

--- k8li.sh
+++ k8li.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-source funcs.sh
-source constants.sh
+# Get the directory of the current script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# Use that directory to source the other scripts
+source "$DIR/funcs.sh"
+source "$DIR/constants.sh"
 
 parse_arguments "$@"
 
@@ -11,6 +15,5 @@ select_resource
 select_action
 
 name_fzf
-
 
 execute_action
